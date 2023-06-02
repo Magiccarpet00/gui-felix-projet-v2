a6469dc38bd5eff5ac4e94a9f14263b239a7a928
Félix Tellitocci <73948501+Magiccarpet00@users.noreply.github.com>
2023-06-02 23:44:30 +0200

    camera follow

--- Assets/MainCharacter/Ennemies/DeplacementEnnemy.cs
+++ Assets/MainCharacter/Ennemies/DeplacementEnnemy.cs
@@ -10,14 +10,13 @@ public class DeplacementEnnemy : MonoBehaviour
 
     [SerializeField] float walkDistance = 7f, attackDistance = 1f; // ajouter une variable idleDistance = 10f pour l'animation d'idle
 
-    // Start is called before the first frame update
     void Start()
     {
         //target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         StartCoroutine(delayTricks());
     }
 
-    public IEnumerator delayTricks()
+    public IEnumerator delayTricks() //pas super important le petit delay
     {
         yield return new WaitForSeconds(1);
         //ennemyAgent.SetDestination(new Vector3(0,0,0));
@@ -25,18 +24,16 @@ public class DeplacementEnnemy : MonoBehaviour
         StartCoroutine(delayTricks());
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //if (ennemyAgent.remainingDistance > walkDistance || ennemyAgent.remainingDistance < attackDistance)
-        //{
-        //    ennemyAgent.speed = 0;
-        //}
-        //else 
-        //{
-        //    ennemyAgent.speed = 1f;
-        //}
-
+        if (ennemyAgent.remainingDistance > walkDistance || ennemyAgent.remainingDistance < attackDistance)
+        {
+            ennemyAgent.speed = 0;
+        }
+        else
+        {
+            ennemyAgent.speed = 1f;
+        }
 
         //ennemyAgent.SetDestination(target.position);
     }
